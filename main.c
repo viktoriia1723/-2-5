81a5e19a4263ba696c167981a02cbec76cd13973
viktoriia1723 <krashchenko.viktoriia@student.sumdu.edu.ua>
2025-04-09 22:09:42 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
+    int distance1 = 120; // км
+    int time1 = 2;      // години
+    int distance2 = 80;  // км
+    int time2 = 1;      // година
     
-    printf("Завдання 1: Поміняти місцями два числа без третьої змінної\n");
-    printf("Введіть перше число: ");
-    scanf("%d", &a);
-    printf("Введіть друге число: ");
-    scanf("%d", &b);
+    int totalDistance = distance1 + distance2;
+    int totalTime = time1 + time2;
+    float averageSpeed = (float)totalDistance / totalTime;
     
-    printf("До обміну: a = %d, b = %d\n", a, b);
-    
-    // Обмін значень без використання третьої змінної
-    a = a + b;
-    b = a - b;
-    a = a - b;
-    
-    printf("Після обміну: a = %d, b = %d\n", a, b);
+    printf("Завдання 4: Обчислення середньої швидкості\n");
+    printf("Пройдена відстань: %d км\n", totalDistance);
+    printf("Витрачений час: %d годин\n", totalTime);
+    printf("Середня швидкість: %.2f км/год\n", averageSpeed);
     
     return 0;
 }
